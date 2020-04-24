cf48bd4a978d7db847a2ecf7c28e918004c7cf1e
WSx41 <39915191+WSx41@users.noreply.github.com>
2020-04-24 19:58:38 -0300

    Add files via upload

---
+++ iscript.sh
@@ -0,0 +1,264 @@
+#!/bin/bash
+#Version 8.2 - March 2019
+#Written by Augusto Veiga
+#Edited by Falconi
+#Edited by Weslley
+
+bold=$(tput bold)
+
+function help() {
+
+	##############################################
+	# Banner do script e Funcao HELP do script   #
+	##############################################
+	echo ""
+	echo "  @@@@@@   @@@@@@     @@@@@@   @@@@@@@   @@@@@@  @@@@@@@   @@@@@@@@"
+    	echo "    @@    @@    @@   @@    @@  @@    @@    @@    @@    @@     @@"
+    	echo "    @@    @@         @@        @@    @@    @@    @@    @@     @@"
+    	echo "    @@     @@@@@@    @@        @@@@@@@     @@    @@@@@@@      @@"
+    	echo "    @@          @@   @@        @@    @@    @@    @@           @@"
+    	echo "    @@    @@    @@   @@    @@  @@    @@    @@    @@           @@"
+    	echo "  @@@@@@   @@@@@@     @@@@@@   @@    @@  @@@@@@  @@           @@"
+	echo ""
+	echo "iscript - Simple script to run NMAP + NIKTO + SSLSCAN TEST."
+	echo ""
+	echo "-i to input a IP or many IPs as a target - eg: 192.168.1.1 or 192.168.1.1/24 or 192.168.1.1-100"
+	echo "-l to input a file with target(s)"
+	echo "--help to show help menu"
+	echo ""
+
+}
+
+if [ $1 ] # Condicao para utilizar os argumentos -i e -l
+then
+	case "$1" in 
+	-i) # Argumento para aceitar o input de um IP, Range ou Barramento
+
+	##############################################
+	# Banner do script                           #
+	##############################################
+	echo ""
+	echo "  @@@@@@   @@@@@@     @@@@@@   @@@@@@@   @@@@@@  @@@@@@@   @@@@@@@@"
+	echo "    @@    @@    @@   @@    @@  @@    @@    @@    @@    @@     @@"
+	echo "    @@    @@         @@        @@    @@    @@    @@    @@     @@"
+	echo "    @@     @@@@@@    @@        @@@@@@@     @@    @@@@@@@      @@"
+	echo "    @@          @@   @@        @@    @@    @@    @@           @@"
+	echo "    @@    @@    @@   @@    @@  @@    @@    @@    @@           @@"
+	echo "  @@@@@@   @@@@@@     @@@@@@   @@    @@  @@@@@@  @@           @@"
+
+	################################################
+	# Cria a estrutura de diretorios e copia o     #
+	# nmap.xsl para a pasta do NMAP                #
+	################################################
+	mkdir /root/Desktop/iscriptResult
+	mkdir /root/Desktop/iscriptResult/NMAP
+	mkdir /root/Desktop/iscriptResult/SSLSCAN
+	mkdir /root/Desktop/iscriptResult/NIKTO
+	mkdir /root/Desktop/iscriptResult/LOG
+	mkdir /root/Desktop/iscriptResult/PRINTS
+	cp /usr/share/nmap/nmap.xsl /root/Desktop/iscriptResult/NMAP
+
+	echo ""
+
+	echo -e "\e[1;31m${bold}UPLIST - NMAP\e[0m"
+	#################################################
+	# Port scan para identificar os hosts que estao #
+	# UP.                                           #    
+	#################################################
+	nmap -sn -vv -T4 $2 -oG /root/Desktop/iscriptResult/NMAP/pingscan | tee /root/Desktop/iscriptResult/LOG/pingscan.log
+
+	#################################################
+	# Grep para criar um arquivo com os hosts UPs   #
+	#################################################
+	grep Up /root/Desktop/iscriptResult/NMAP/pingscan | awk '{print $2}' > /root/Desktop/iscriptResult/NMAP/uplist
+
+	echo ""
+
+	echo -e "\e[1;31m${bold}NMAP SCAN - PORT SCANNER + BANNER\e[0m"
+	#################################################
+	# NMAP - Port Scanner                           #
+	# 3 OUTPUTS + LOG                               #
+	#################################################
+	nmap -sV -sC --open -sSU -vv $2 --stylesheet nmap.xsl -oA /root/Desktop/iscriptResult/NMAP/nmap_result | tee /root/Desktop/iscriptResult/LOG/nmap_result.log
+
+	################################################
+	# Grep para criar os arquivos "port80' e       #
+	# "port443" a partir do NMAP - Arquivos serao  #
+	# utilizados ao decorrer do script             #
+	################################################
+	grep 80/open /root/Desktop/iscriptResult/NMAP/nmap_result.gnmap | awk '{print $2}' > /root/Desktop/iscriptResult/NMAP/port80
+	grep 443/open /root/Desktop/iscriptResult/NMAP/nmap_result.gnmap | awk '{print $2}' > /root/Desktop/iscriptResult/NMAP/port443
+	
+	echo ""
+
+	echo -e "\e[1;31m${bold}SSLSCAN\e[0m"
+	################################################
+	# Comando para executar o SSLCAN               #
+	################################################
+	sslscan --no-failed --show-certificate --xml=/root/Desktop/iscriptResult/SSLSCAN/$2 --targets=/root/Desktop/iscriptResult/NMAP/port443 | tee /root/Desktop/iscriptResult/LOG/SSLSCAN.log
+
+	echo ""
+
+	echo -e "\e[1;31m${bold}NIKTO SCAN\e[0m"
+	################################################
+	# NIKTO - Criamos os FORs para rodar o comando #
+	# em todos os IPs que estao nos arquivos       #
+	# "port80" e "port443"                         #
+	################################################
+	for ip in `cat /root/Desktop/iscriptResult/NMAP/port80`
+		do 
+			nikto -host $ip -port 80 -ask no -Format HTML -o /root/Desktop/iscriptResult/NIKTO/result_nikto.html | tee /root/Desktop/iscriptResult/LOG/nikto80.log
+		done
+
+	for ip in `cat /root/Desktop/iscriptResult/NMAP/port443`
+		do 
+			nikto -host $ip -port 443 -ask no -Format HTML -o /root/Desktop/iscriptResult/NIKTO/result_nikto.html | tee /root/Desktop/iscriptResult/LOG/nikto443.log
+		done
+	
+	# Gerar o arquivo final para realizar a integração com o script de print.
+	sed 's/^/https:\/\//g' /root/Desktop/iscriptResult/NMAP/port443 | sed 's/$/:443/g' > /root/Desktop/iscriptResult/final_result
+	sed 's/^/http:\/\//g' /root/Desktop/iscriptResult/NMAP/port80 | sed 's/$/:80/g' >> /root/Desktop/iscriptResult/final_result
+	
+	
+	###############################################
+	# Mover os arquivos da pasta NMAP para a      #
+	# LOG                                         #
+	###############################################
+	mv /root/Desktop/iscriptResult/NMAP/uplist /root/Desktop/iscriptResult/LOG
+	mv /root/Desktop/iscriptResult/NMAP/pingscan /root/Desktop/iscriptResult/LOG
+	mv /root/Desktop/iscriptResult/NMAP/port80 /root/Desktop/iscriptResult/LOG
+	mv /root/Desktop/iscriptResult/NMAP/port443 /root/Desktop/iscriptResult/LOG
+	
+	# Integração
+	/iscript/./printscreen.sh -l /root/Desktop/iscriptResult/final_result
+	
+
+	;;
+
+	-l) # Argumento para aceitar como input um arquito
+	if [[ -e $2 ]] #Condicao para tratar um arquivo como input
+	then
+		##############################################
+		# Banner do script                           #
+		##############################################
+		echo ""
+        	echo "  @@@@@@   @@@@@@     @@@@@@   @@@@@@@   @@@@@@  @@@@@@@   @@@@@@@@"
+        	echo "    @@    @@    @@   @@    @@  @@    @@    @@    @@    @@     @@"
+       		echo "    @@    @@         @@        @@    @@    @@    @@    @@     @@"
+        	echo "    @@     @@@@@@    @@        @@@@@@@     @@    @@@@@@@      @@"
+        	echo "    @@          @@   @@        @@    @@    @@    @@           @@"
+        	echo "    @@    @@    @@   @@    @@  @@    @@    @@    @@           @@"
+        	echo "  @@@@@@   @@@@@@     @@@@@@   @@    @@  @@@@@@  @@           @@"
+		echo ""
+
+		################################################
+		# Cria a estrutura de diretorios e copia o     #
+		# nmap.xsl para a pasta do NMAP                #
+		################################################
+		mkdir /root/Desktop/iscriptResult
+        	mkdir /root/Desktop/iscriptResult/NMAP
+        	mkdir /root/Desktop/iscriptResult/SSLSCAN
+        	mkdir /root/Desktop/iscriptResult/NIKTO
+        	mkdir /root/Desktop/iscriptResult/LOG
+		mkdir /root/Desktop/iscriptResult/PRINTS
+		cp /usr/share/nmap/nmap.xsl /root/Desktop/iscriptResult/NMAP
+
+		echo ""
+
+		echo -e "\e[1;31m${bold}UPLIST - NMAP\e[0m"
+		#################################################
+		# Port scan para identificar os hosts que estao #
+		# UP.                                           #    
+		#################################################
+		nmap -Pn -sn -vv -T4 -iL $2 -oG /root/Desktop/iscriptResult/NMAP/pingscan | tee /root/Desktop/iscriptResult/LOG/pingscan.log
+
+		#################################################
+		# Grep para criar um arquivo com os hosts UPs   #
+		#################################################
+		grep Up /root/Desktop/iscriptResult/NMAP/pingscan | awk '{print $2}' > /root/Desktop/iscriptResult/NMAP/uplist
+		
+		echo ""
+
+		echo -ne "\n\e[1;31m${bold}NMAP SCAN - PORT SCANNER + BANNER\e[0m"
+		#################################################
+		# NMAP - Port Scanner                           #
+		# 3 OUTPUTS + LOG                               #
+		#################################################
+		nmap -Pn -sS -sV --script discovery -iL /root/Desktop/iscriptResult/NMAP/uplist --stylesheet nmap.xsl -oA /root/Desktop/iscriptResult/NMAP/nmap_result | tee /root/Desktop/iscriptResult/LOG/nmap_result.log
+
+		################################################
+		# Grep para criar os arquivos "port80' e       #
+		# "port443" a partir do NMAP - Arquivos serao  #
+		# utilizados ao decorrer do script             #
+		################################################
+		grep 80/open /root/Desktop/iscriptResult/NMAP/nmap_result.gnmap | awk '{print $2}' > /root/Desktop/iscriptResult/NMAP/port80
+		grep 443/open /root/Desktop/iscriptResult/NMAP/nmap_result.gnmap | awk '{print $2}' > /root/Desktop/iscriptResult/NMAP/port443
+
+		echo ""
+
+		echo -ne "\n\e[1;31m${bold}SSLSCAN\e[0m\n"
+		################################################
+		# Comando para executar o SSLCAN               #
+		################################################
+		sslscan --no-failed --show-certificate --xml=/root/Desktop/iscriptResult/SSLSCAN/$2 --targets=/root/Desktop/iscriptResult/NMAP/port443 | tee /root/Desktop/iscriptResult/LOG/SSLSCAN.log
+
+		echo ""
+
+		echo -ne "\n\e[1;31m${bold}NIKTO SCAN\e[0m\n"
+		################################################
+		# NIKTO - Criamos os FORs para rodar o comando #
+		# em todos os IPs que estao nos arquivos       #
+		# "port80" e "port443"                         #
+		################################################
+		for ip in `cat /root/Desktop/iscriptResult/NMAP/port80`
+			do 
+				nikto -host $ip -port 80 -ask no -Format HTML -o /root/Desktop/iscriptResult/NIKTO/result_nikto.html | tee /root/Desktop/iscriptResult/LOG/nikto80.log
+			done
+		for ip in `cat /root/Desktop/iscriptResult/NMAP/port443`
+			do 
+				nikto -host $ip -port 443 -ask no -Format HTML -o /root/Desktop/iscriptResult/NIKTO/result_nikto.html | tee /root/Desktop/iscriptResult/LOG/nikto443.log
+			done
+
+		# Gerar o arquivo final para realizar a integração com o script de print.
+		sed 's/^/https:\/\//g' /root/Desktop/iscriptResult/NMAP/port443 | sed 's/$/:443/g' > /root/Desktop/iscriptResult/final_result
+		sed 's/^/http:\/\//g' /root/Desktop/iscriptResult/NMAP/port80 | sed 's/$/:80/g' >> /root/Desktop/iscriptResult/final_result
+		echo "Arquivo final_result criado."
+		echo "Iniciando integração com PRINTSCREEN"
+
+		# Integração
+		/root/Desktop/Iscript/./printscreen.sh -l /root/Desktop/iscriptResult/final_result
+
+		
+		###############################################
+		# Mover os arquivos da pasta NMAP para a      #
+		# LOG                                         #
+		###############################################
+		mv /root/Desktop/iscriptResult/NMAP/uplist /root/Desktop/iscriptResult/LOG
+		mv /root/Desktop/iscriptResult/NMAP/pingscan /root/Desktop/iscriptResult/LOG
+		mv /root/Desktop/iscriptResult/NMAP/port80 /root/Desktop/iscriptResult/LOG
+		mv /root/Desktop/iscriptResult/NMAP/port443 /root/Desktop/iscriptResult/LOG
+		echo "##############################################################"		
+		echo "# ALL DONE! RESULT CAN BE SEE IN /root/Desktop/iscriptResult #"
+		echo "##############################################################"		
+
+	else
+
+		help;
+
+	fi
+
+	;;
+
+	--help)
+
+		help;
+
+	;;
+
+	esac
+
+else
+
+	help;
+
+fi
---
+++ printscreen.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+# Version: 0.0.5
+# Track change last updated by: Falconi - junho 2017 (v0.0.6)
+
+SCRIPT=`basename ${BASH_SOURCE[0]}`
+
+#Variables definition
+OPT_F=png
+com=cutycapt
+prot=http
+check=1
+
+#Set fonts for Help.
+NORM=`tput sgr0`
+BOLD=`tput bold`
+REV=`tput smso`
+
+#Help function
+function HELP {
+  echo -e \\n"Help documentation for ${BOLD}${SCRIPT}.${NORM}"\\n
+  echo -e "${REV}Basic usage:${NORM} ${BOLD}$SCRIPT -i exemple.com -o exemple${NORM}"\\n
+  echo "${REV}-i${NORM}  --Sets the value for a single ${BOLD}ip${NORM}. e.g. http://192.168.0.1:8080 or http://192.168.0.1"
+  echo "${REV}-p${NORM}  --Sets the value for port ${BOLD}ip${NORM}. e.g. 8080 or 8080,8008"
+  echo "${REV}-l${NORM}  --Sets the value for a list of ${BOLD}ips${NORM}. e.g. ./Desktop/test/list"
+  echo "${REV}-o${NORM}  --Sets the value for output ${BOLD}dir${NORM}. e.g. ./Desktop/output"
+  echo "${REV}-f${NORM}  --Sets the value for format ${BOLD}d${NORM}. Default is ${BOLD}png${NORM}. Formats accepted are png|pdf|ps|svg|jpeg"
+  echo -e "${REV}-h${NORM}  --Displays this help message. No further functions are performed."\\n
+  exit 1
+}
+
+if [ -z $1 ]
+then
+HELP
+exit 2
+fi
+while getopts :i:l:p:o:f:h FLAG; do
+  case $FLAG in
+    i)
+      OPT_I=$OPTARG
+      url=$OPT_I
+      ;;
+    l)
+	url=`cat $OPTARG`
+      ;;
+    p)
+      OPT_P=$OPTARG
+      IFS=', ' read -r -a array <<< "$OPT_P"
+      check=2
+     ;;
+    o)
+      OPT_O=$OPTARG
+      check=3 
+     ;;
+    f)
+      OPT_F=$OPTARG
+      ;;
+    h)  #show help
+      HELP
+      ;;
+    \?) #unrecognized option - show help
+      echo -e \\n"Option -${BOLD}$OPTARG${NORM} not allowed."
+      HELP
+      ;;
+  esac
+done
+shift $((OPTIND-1))
+ 
+	function PRINT {
+	for x in $url; do
+		target=`echo $x | sed 's/^http:\/\///' | sed 's/^https:\/\///'`
+		echo "Capturing $target"
+		$com --insecure --delay=10000 --url=$x --out=$target.$OPT_F >/dev/null 2>&1
+		wget $x --dns-timeout=10 --connect-timeout=20 --force-html -o $target.txt
+		mkdir /root/Desktop/PRINTS_SITES
+		mv *.png index.* $target.txt /root/Desktop/PRINTS_SITES/ >/dev/null 2>&1
+	done
+exit 1
+}
+	function PRINT2 {
+        for element in "${array[@]}"; do
+		target=$(echo "$url:$element")
+		echo "Capturing $target"
+        	$com --insecure --delay=10000 --url=$target --out=$target.$OPT_F >/dev/null 2>&1
+                wget $target --dns-timeout=10 --connect-timeout=20 --force-html --output-file=$target.txt
+		mkdir /root/Desktop/PRINTS_SITES
+		mv *.png index.* $target.txt /root/Desktop/PRINTS_SITES/ >/dev/null 2>&1
+	done
+exit 1
+}
+
+	function PRINT3 {
+        for element in "${array[@]}"; do
+        	target=$(echo "$url:$element")
+        	echo "Capturing $target"
+		$com --insecure --delay=10000 --url=$target --out=$OPT_O.$OPT_F >/dev/null 2>&1
+		wget $target --dns-timeout=10 --connect-timeout=20 --force-html --output-file=$target.txt
+		mkdir /root/Desktop/PRINTS_SITES
+		mv *.png index.* $target.txt /root/Desktop/PRINTS_SITES/ >/dev/null 2>&1
+        done
+exit 1
+}
+
+	function MAIN {
+	case $check in
+         1)
+ 		PRINT
+	 ;;
+	 2) 
+		PRINT2
+	 ;;
+	 3)
+		PRINT3
+	 ;;
+esac
+}
+
+
+MAIN
